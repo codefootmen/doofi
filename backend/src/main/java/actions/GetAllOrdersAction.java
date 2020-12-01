4d1ae570b438c6b16a29a50287ff86e9266dee10
Flavio Barci <flavio.barci@gmail.com>
2020-12-01 01:22:50 -0300

    Use Bag

--- backend/src/main/java/actions/GetAllOrdersAction.java
+++ backend/src/main/java/actions/GetAllOrdersAction.java
@@ -1,9 +1,11 @@
 package actions;
 
+import model.BagManager;
 import model.Client;
 import model.Order;
 import persistence.Dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GetAllOrdersAction implements ICommand {
@@ -13,6 +15,9 @@ public class GetAllOrdersAction implements ICommand {
         Order order = Order.builder().build();
 
         List<Order> orderList = Dao.getInstance().getAll(order);
+
+        BagManager.bag.setOrders((ArrayList<Order>) orderList);
+        BagManager.bag.snapshot();
 
         return orderList;
     }
